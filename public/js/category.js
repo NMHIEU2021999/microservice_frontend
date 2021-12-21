ac85572395ddda6c2180c9bf11fbb76086b26f22
NMHIEU2021999 <minhhieu20219992@gmail.com>
2021-12-21 20:28:19 +0700

    test

--- public/js/category.js
+++ public/js/category.js
@@ -25,26 +25,25 @@ $('#add-product').on('submit', function(e){
     data.images = [];
     data.images.push($("#img1").attr("src").split('base64,')[1]);
     data.images.push($("#img2").attr("src").split('base64,')[1]);
-    data.tag = [];
+    let tags = [];
     $('.add_checkbox input:checked').each(function() {
-        data.tag.push($(this).attr('name'));
+        tags.push($(this).attr('name'));
     });
     console.log(data)
-    // $.ajax({
-    //     url: "addresses",
-    //     type: "POST",
-    //     async: false,
-    //     data: JSON.stringify(data),
-    //     success: function (data, textStatus, jqXHR) {
-    //         location.reload();
-    //     },
-    //     error: function (jqXHR, textStatus, errorThrown) {
-    //         $("#user-message").html('<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button> Error saving the address. ' + errorThrown + '</div>');
-    //         console.log('error: ' + JSON.stringify(jqXHR));
-    //         console.log('error: ' + textStatus);
-    //         console.log('error: ' + errorThrown);
-    //     },
-    // });
-    debugger
-    console.log("abcd")
+    console.log(tags)
+    $.ajax({
+        url: "/catalogue",
+        type: "POST",
+        async: false,
+        data: JSON.stringify(data),
+        success: function (data, textStatus, jqXHR) {
+            location.reload();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(errorThrown);
+            console.log('error: ' + JSON.stringify(jqXHR));
+            console.log('error: ' + textStatus);
+            console.log('error: ' + errorThrown);
+        },
+    });
   });

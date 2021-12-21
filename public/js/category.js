b54295beaa629235a4d28419dcfff5ea8fb866d5
NMHIEU2021999 <minhhieu20219992@gmail.com>
2021-12-21 20:21:02 +0700

    test

--- public/js/category.js
+++ public/js/category.js
@@ -22,6 +22,13 @@ $('#add-product').on('submit', function(e){
     data.price = $("#add-price").val();
     data.count = $("#add-count").val();
     data.description = $("#add-description").val();
+    data.images = [];
+    data.images.push($("#img1").attr("src").split('base64,')[1]);
+    data.images.push($("#img2").attr("src").split('base64,')[1]);
+    data.tag = [];
+    $('.add_checkbox input:checked').each(function() {
+        selected.push($(this).attr('name'));
+    });
     console.log(data)
     // $.ajax({
     //     url: "addresses",

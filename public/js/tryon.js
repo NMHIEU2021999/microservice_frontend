e6c16446196d9266c7a3e7e1a3fa5cb03fda7b82
ntd275 <ntd275@gmail.com>
2021-12-22 21:14:23 +0700

    add

---
+++ public/js/tryon.js
@@ -0,0 +1,49 @@
+openTryOn = () => {
+    $('#modalTryOn').modal('show');
+}
+
+closeTryOn = () => {
+    $('#modalTryOn').modal('hide');
+    $('#tryOnForm').trigger("reset");
+    $("#img1").hide();
+    $("#img2").hide();
+}
+
+$('#try-on-button').on('click',()=>{
+    openTryOn()
+})
+
+function getBase64Image(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    var dataURL = canvas.toDataURL("image/png");
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
+
+$('#tryOnForm').on('submit', function (e) {
+    e.preventDefault();
+    let data = {
+        per_img: $("#img1").attr("src").split('base64,')[1],
+        clo_img: getBase64Image(document.getElementById("imageMain")),
+    };
+    console.log(data);
+
+    // $.ajax({
+    //     url: "try-on",
+    //     type: "POST",
+    //     async: true,
+    //     data: JSON.stringify(data),
+    //     success: function (data, textStatus, jqXHR) {
+    //         location.reload();
+    //     },
+    //     error: function (jqXHR, textStatus, errorThrown) {
+    //         alert(errorThrown);
+    //         console.log('error: ' + JSON.stringify(jqXHR));
+    //         console.log('error: ' + textStatus);
+    //         console.log('error: ' + errorThrown);
+    //     },
+    // });
+});

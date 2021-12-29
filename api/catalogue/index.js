f294a5cc35d5edd92da7f2bc0e0d3f80f7163eba
ntd275 <ntd275@gmail.com>
2021-12-29 22:39:00 +0700

    a

--- api/catalogue/index.js
+++ api/catalogue/index.js
@@ -17,19 +17,12 @@
       .pipe(res);
   });
 
-  app.get("/catalogue*", function (req, res, next) {
-    // helpers.simpleHttpRequest(
-    //   endpoints.catalogueUrl + req.url.toString(),
-    //   res,
-    //   next
-    // );
-    var url = endpoints.catalogueUrl + req.url.toString();
-    request
-      .get(url)
-      .on("error", function (e) {
-        next(e);
-      })
-      .pipe(res);
+  app.get("/catalogue", function (req, res, next) {
+    helpers.simpleHttpRequest(
+      endpoints.catalogueUrl + req.url.toString(),
+      res,
+      next
+    );
   });
 
   app.post("/catalogue*", function (req, res, next) {
